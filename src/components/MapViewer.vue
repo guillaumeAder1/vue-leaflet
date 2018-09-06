51ac2997cf414e20983360c29681436ced913dfa
Guillaume Ader <GAder@esriuk.com>
2018-09-06 17:09:05 +0100

    formatting html

--- src/components/MapViewer.vue
+++ src/components/MapViewer.vue
@@ -1,33 +1,30 @@
 <template>
-    <l-map
-      style="height:450px;"
-     :zoom="zoom" 
-     :center="center">
-      <l-tile-layer :url="url" ></l-tile-layer>
-      <l-marker :lat-lng="marker"></l-marker>
-    </l-map>
+  <l-map style="height:450px;" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url"></l-tile-layer>
+    <l-marker :lat-lng="marker"></l-marker>
+  </l-map>
 </template>
 
 <script>
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 export default {
-  props: ["height"],
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker
-  },
-  data() {
-    return {
-      zoom: 13,
-      center: L.latLng(47.41322, -1.219482),
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-      marker: L.latLng(47.41322, -1.219482)
-    };
-  },
-  mounted() {
-    console.log(this.$refs);
-  }
+    props: ["height"],
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker
+    },
+    data() {
+        return {
+            zoom: 13,
+            center: L.latLng(47.41322, -1.219482),
+            url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+            marker: L.latLng(47.41322, -1.219482)
+        };
+    },
+    mounted() {
+        console.log(this.$refs);
+    }
 };
 </script>
 
