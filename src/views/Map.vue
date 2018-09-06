f4b380242454256a9b38240879b1c7965ea6753c
Guillaume Ader <GAder@esriuk.com>
2018-09-06 16:58:27 +0100

    fix L.Icon.Default.prototype._getIconUrl issue and create Map.vue as separate chunk

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="map-container">
-           
-      
-    <el-container >
-        <el-aside>
-          <!-- {{$store.state.msg}} -->
-          <Routes/>
-        </el-aside>
-        <el-main>
-            <MapViewer/>
-        </el-main>
+
+    <el-container>
+      <el-aside>
+        <!-- {{$store.state.msg}} -->
+        <Routes/>
+      </el-aside>
+      <el-main>
+        <MapViewer/>
+      </el-main>
     </el-container>
   </div>
 </template>
@@ -17,21 +16,29 @@
 <script>
 import MapViewer from "@/components/MapViewer.vue";
 import Routes from "@/components/Routes.vue";
+import "leaflet/dist/leaflet.css";
 
+// hack to fix marker icon not displayed
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 export default {
-  components: {
-    MapViewer,
-    Routes
-  }
+    components: {
+        MapViewer,
+        Routes
+    }
 };
 </script>
 
 
 <style scoped lang="scss">
 .el-main {
-  padding: 0;
+    padding: 0;
 }
 .el-aside {
-  height: 450px;
+    height: 450px;
 }
 </style>
